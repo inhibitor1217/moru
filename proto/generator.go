11f3525f7cb7e7a05ef3c69d3e4b3a321e67425e
inhibitor1217 <inhibitor@kaist.ac.kr>
2025-03-02 16:48:52 +0900

    feat(proto): define FFI interface for KnownPeers()

--- proto/generator.go
+++ proto/generator.go
@@ -1,3 +1,3 @@
 package proto
 
-//go:generate protoc --go_out=. --go_opt=module=github.com/inhibitor1217/moru/proto discovery/message.proto discovery/peer.proto
+//go:generate protoc --go_out=. --go_opt=module=github.com/inhibitor1217/moru/proto discovery/ffi.proto discovery/message.proto discovery/peer.proto
